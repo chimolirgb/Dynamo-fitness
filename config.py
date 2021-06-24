6dc814dcc2080f1e6b7b7ef5a7da113fcbc30909
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-25 00:57:31 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -18,8 +18,9 @@ class Config():
     
 
 class ProdConfig(Config):
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:dynamo@localhost/dynamo'
