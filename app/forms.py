dbde90c2d2dcd33af293413587f4a1ab22247b85
ephraim-kamau54 <ephraim.kamau@student.moringaschool.com>
2021-06-24 12:20:54 +0300

    Initialcommits

---
+++ app/forms.py
@@ -0,0 +1,18 @@
+from wtforms import Form, TextField, TextAreaField, SubmitField, validators, ValidationError
+
+class ContactForm(Form):
+  name = TextField("Name")
+  email = TextField("Email")
+  subject = TextField("Subject")
+  message = TextAreaField("Message")
+  submit = SubmitField("Send")
+
+
+class LoginForm(Form):
+  name = TextField("Name",  [validators.Required("Please enter your name.")])
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  subject = TextField("Subject",  [validators.Required("Please enter a subject.")])
+  message = TextAreaField("Message",  [validators.Required("Please enter a message.")])
+  submit = SubmitField("Send")
+
+
