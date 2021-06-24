d2ef8fccc6c0d370bf432338f3b4d274171b1ab6
ephraim-kamau54 <ephraim.kamau@student.moringaschool.com>
2021-06-24 12:23:56 +0300

    newflies

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,25 +5,3 @@ from flask import render_template,redirect,url_for, flash,request
 from flask_login import login_user,login_required,logout_user
 from . import auth 
  
-@auth.route('/contact', methods=["GET", "POST"])
-def contact():
-    form = ContactForm()
-
-    if form.validate_on_submit():
-
-        user = UserAgentMixin.query.filter_by(email=form.email.data).first()
-
-        if user is not None and user.verify_password(form.password.data):
-
-            form (user, form.remember.data)
-
-            if user.is_admin:
-                return redirect(url_for('main.admin_dashboard'))
-            else:
-                return redirect(request.args.get('next') or url_for('main.index'))
-
-        flash('Invalid username or password')
-
-    title = "Dynamo-fitness contact"
-
-    return render_template('auth/contact.html', contact_form=form, title=title)
--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,9 @@
 
+from flask.helpers import flash
+from flask.wrappers import Request
+from werkzeug.utils import redirect
+from werkzeug.wrappers import UserAgentMixin
+from app.forms import ContactForm
 from . import main
 from flask import render_template,url_for
 from .. import db,photos
@@ -10,3 +15,27 @@ from .. import db,photos
 def index():
 
    return render_template('index.html' )
+
+@main.route('/contact', methods=["GET", "POST"])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+
+        user = UserAgentMixin.query.filter_by(email=form.email.data).first()
+
+        if user is not None and user.verify_password(form.password.data):
+
+            form (user, form.remember.data)
+
+            if user.is_admin:
+                return redirect(url_for('main.admin_dashboard'))
+            else:
+                return redirect(Request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or password')
+
+    title = "Dynamo-fitness contact"
+
+    return render_template('auth/contact.html', contact_form=form, title=title)
+  
